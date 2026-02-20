5c444406d16e227d3253ac21084bad257322b4ec
Minseong <akaza@Minseongui-MacBookAir.local>
2026-02-20 11:58:26 +0900

    説明など追加

--- bootstrap/oidc.tf
+++ bootstrap/oidc.tf
@@ -1,13 +1,18 @@
 provider "aws" {
   region = "ap-northeast-1" # 東京
+  profile = "project_1"
 }
 
 # 1. GitHub Actionsを利用するためのOIDC(OpenID Connect)資格証明生成
 resource "aws_iam_openid_connect_provider" "github" {
+  # 身分証を発行する公式のurl
   url             = "https://token.actions.githubusercontent.com"
+  # 身分証を受け取る対象(sts : Security Token Service)
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612", "6938fd4d98bab03faadb97b34396831e3780aea1"] 
+  # 通信の安全性を担保するための、github側のSSL証明書のサムプリント（指紋）
   # (最新 GitHub OIDC 証明書 2個)
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612", "6938fd4d98bab03faadb97b34396831e3780aea1"] 
+  
 }
 
 # 2. GitHub パイプラインが使う　IAM Role生成
