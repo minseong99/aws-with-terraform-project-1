5f0796b635579249d5bfe7586cf3e302639a1e7f
Minseong <akaza@Minseongui-MacBookAir.local>
2026-02-22 21:45:25 +0900

    feat: ALB構築およびALBのCI／CD追加

---
+++ workspace/dev/alb.tf
@@ -0,0 +1,60 @@
+# ==============================================================================
+# 1. Application Load Balancer 
+# ==============================================================================
+resource "aws_lb" "main" {
+  count = var.enable-compute ? 1 : 0
+
+  name               = "${var.environment}-alb"
+  internal           = false # 外部のインターネット通信のため
+  load_balancer_type = "application"
+
+  # 앞서 만든 ALB 전용 방화벽 장착
+  security_groups = [aws_security_group.alb-sg.id]
+
+  subnets = [
+    aws_subnet.public-subnets["public-subnet-a"].id,
+    aws_subnet.public-subnets["public-subnet-b"].id
+  ]
+
+  tags = {
+    Name = "${var.environment}-alb"
+  }
+}
+
+# ==============================================================================
+# 2. Target Group (ターゲットグループ )
+# ==============================================================================
+
+resource "aws_lb_target_group" "web" {
+  name     = "${var.environment}-web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  #　health check: ３０秒ごとにサーバが生きているか確認
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    matcher             = "200"
+  }
+}
+
+# ==============================================================================
+# 3. Listener (リスナ)
+# ==============================================================================
+resource "aws_lb_listener" "http" {
+  count = var.enable-compute ? 1 : 0
+
+  load_balancer_arn = aws_lb.main[0].arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  # target groupにtrafficを送る
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
