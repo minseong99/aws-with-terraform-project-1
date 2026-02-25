ab133ec5f3be8bb14a10145262ca12c66d8b9ed3
Minseong <akaza@Minseongui-MacBookAir.local>
2026-02-25 19:16:31 +0900

    refactoring : directoryの構造変更

---
+++ env/prod/main.tf
@@ -0,0 +1,31 @@
+
+provider "aws" {
+  region = "ap-northeast-1"
+  # profile = var.aws-profile
+
+  default_tags {
+    tags = {
+      Project     = "project_1"
+      Environment = "prod"
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "my-unique-tfstate-bucket-minseong99-b9uqxxnj"
+    region         = "ap-northeast-1"
+    dynamodb_table = "my-tf-lock-table"
+    key            = "prod/terraform.tfstate"
+    encrypt        = true
+  }
+}
+
+module "dev-infra" {
+  source = "../../modules/project1_infra"
+
+  aws-region = "ap-northeast-1" 
+  environment = "prod"
+}
--- modules/project1_infra/variables.tf
+++ modules/project1_infra/variables.tf
@@ -1,19 +1,18 @@
 variable "aws-region" {
   description = "The main region for aws"
   type        = string
-  default     = "ap-northeast-1" # tokyo
+  # default     = "ap-northeast-1" # tokyo
 }
 
-variable "aws-profile" {
-  description = "This is profile for aws credentials"
-  type        = string
-  default     = "project_1"
-}
+# variable "aws-profile" {
+#   description = "This is profile for aws credentials"
+#   type        = string
+#   # default     = "project_1"
+# }
 
 variable "environment" {
   description = "This is environment value."
   type        = string
-  default     = "dev"
 }
 
 variable "enable-nat-gateway" {
